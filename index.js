88ee1692b60c1d7fa2f56cb966ad6e36a6d08fef
Janice Lam <janice.lam21@gmail.com>
2019-08-07 15:54:07 -0700

    ec2 ready

--- index.js
+++ index.js
@@ -9,25 +9,25 @@ const apiProxy = httpProxy.createProxyServer();
 
 app.use('/:id', express.static(path.join(__dirname)));
 
-// const gallery = 'http://ec2-52-53-207-161.us-west-1.compute.amazonaws.com:3000';
-// const reservation = 'http://ec2-18-217-9-12.us-east-2.compute.amazonaws.com:3001';
+const gallery = 'http://ec2-34-217-16-94.us-west-2.compute.amazonaws.com';
+const reservation = 'http://54.183.216.157';
 // const popular = 'http://ec2-3-95-234-77.compute-1.amazonaws.com';
-const header = 'http://ec2-3-14-71-98.us-east-2.compute.amazonaws.com:3003';
+const header = 'http://ec2-18-188-246-230.us-east-2.compute.amazonaws.com';
 
 app.all("/:id/header", function(req, res) {
   // console.log('redirecting to header');
   apiProxy.web(req, res, {target: header});
 });
 
-// app.all("/reservation/:id", function(req, res) {
-//     console.log('redirecting to reservation');
-//     apiProxy.web(req, res, {target: reservation});
-// });
+app.all(":id/reservation", function(req, res) {
+    console.log('redirecting to reservation');
+    apiProxy.web(req, res, {target: reservation});
+});
 
-// app.all("/gallery/:id", function(req, res) {
-//     console.log('redirecting to gallery');
-//     apiProxy.web(req, res, {target: gallery});
-// });
+app.all("/:id/gallery", function(req, res) {
+    console.log('redirecting to gallery');
+    apiProxy.web(req, res, {target: gallery});
+});
 
 // app.all("/popular/:id", function(req, res) {
 //   console.log('redirecting to popular');
